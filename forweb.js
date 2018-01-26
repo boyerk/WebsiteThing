fbb1a7cb7a68fc4d2d83bf5252546b76f2ef2bd9
Kane <kaboyer@yahoo.com>
2018-01-25 18:46:22 -0800

    added rest of website

---
+++ forweb.js
@@ -0,0 +1,45 @@
+function overMouse() {
+	document.getElementById("changeColor").style.color = "#26ff00";
+}
+function leaveMouse() {
+	document.getElementById("changeColor").innerHTML = "This is Actually Facts about Cars.. Scroll Down";
+}
+function isOverMouse() {
+	document.getElementById("changeHeader").style.color = "#ed5600";
+}
+function isOffMouse() {
+	document.getElementById("changeHeader").innerHTML = "My Name Is KANE I love Cars";
+}
+function grabCar1(){
+	document.getElementById("image").src = "Lexus.jpg";
+}
+function grabCar2(){
+	document.getElementById("car2").src = "LFA.jpg";
+}
+function grabCar3(){
+	document.getElementById("car3").src = "86.jpg";
+}
+function onButton(){
+	document.getElementById("overButton").innerHTML = "REVEAL A COOL CAR";
+}
+function offButton(){
+	document.getElementById("overButton").innerHTML = "click for cool HELICOPTER";
+}
+function onButton2(){
+	document.getElementById("overButton2").innerHTML = "REVEAL ANOTHER COOL CAR";
+}
+function offButton2(){
+	document.getElementById("overButton2").innerHTML = "click for another cool HElICOPTER";
+}
+function onButton3(){
+	document.getElementById("overButton3").innerHTML = "EVEN MORE COOL CARS";
+}
+function offButton3(){
+	document.getElementById("overButton3").innerHTML = "Click for ANOTHER HELICOPTER";
+}
+function onText() {
+	document.getElementById("aboveText").style.color = "#42f4f4";
+}
+function offText() {
+	document.getElementById("aboveText").innerHTML = "An internal combustion engine (ICE) is a heat engine where the combustion of a fuel occurs with an oxidizer (usually air) in a combustion chamber that is an integral part of the working fluid flow circuit. In an internal combustion engine, the expansion of the high-temperature and high-pressure gases produced by combustion applies direct force to some component of the engine. The force is applied typically to pistons, turbine blades, rotor or a nozzle. This force moves the component over a distance, transforming chemical energy into useful mechanical energy.";
+}
